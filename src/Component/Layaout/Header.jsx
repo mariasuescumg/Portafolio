af5d9c80426c0382ae2a198f94045c965ccff372
josesunrise <114879421+mariasuescumg@users.noreply.github.com>
2023-01-12 14:50:45 -0500

    component

--- src/Component/Layaout/Header.jsx
+++ src/Component/Layaout/Header.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div>
+        <img src="" alt="logo" />
+      </div>
+
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
   )
 }
